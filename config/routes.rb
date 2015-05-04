06952980485d502449ece7187211bc1c5741745b
Kohrok <nicf@outlook.com>
2015-05-04 00:40:32 -0400

    Adding a StaticPages controller to handle display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
   resources :players
   resources :leagues
   # The priority is based upon order of creation: first created -> highest priority.
